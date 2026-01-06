f6dbf5d862b94ff31ca2a2440ac9c5bdb8c98182
Fathima Rafah <fathimarafah03@gmail.com>
2026-01-06 16:14:13 +0530

    correct the error

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import AppSidebar from "./Appsidebar"
+import { AppSidebar } from "./Appsidebar"
 import { Outlet } from "react-router-dom";
 
 export function Layout() {
