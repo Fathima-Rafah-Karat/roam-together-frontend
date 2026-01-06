b3d6fb36341cd5c6b1b4f3a105c23e9a569c6d79
Fathima Rafah <fathimarafah03@gmail.com>
2026-01-06 15:50:40 +0530

    correct the error

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/index.jsx";
+import Index from "./pages/Index.jsx";
 import Auth from "./pages/Auth";
 import DiscoverTrips from "./pages/Dashboard";
 import TripDetails from "./pages/TripDetails";
