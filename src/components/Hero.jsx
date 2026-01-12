42c87b0469ecf8046b00a2d243c04469185d3d5f
Fathima Rafah <fathimarafah03@gmail.com>
2026-01-12 16:55:29 +0530

    correct the error

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -234,7 +234,7 @@ const Hero = () => {
                 }
 
                 // Traveler allowed
-                navigate("/dash/dashboard");
+                navigate("/dashboard");
               }}
             >
               <Users className="mr-2 h-5 w-5" />
